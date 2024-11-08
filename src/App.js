b21f728d92fc97fd2ed17a5fba9bb76b385936eb
omraut24 <40856239+omraut24@users.noreply.github.com>
2024-11-09 00:34:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// App.js
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Routes,
+} from "react-router-dom";
+import Home from "../src/page-components/home/index";
+import TopRated from "../src/page-components/top-rated/top-rated";
+import Upcoming from "../src/page-components/upcoming/upcoming";
+import Routestree from "../src/RouterService/Routestree";
+import Details from "./page-components/details/detail";
+import { Navbar } from "./page-components/header/header";
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/toprated" element={<TopRated />} />
+        <Route path="/upcoming" element={<Upcoming />} />
+        <Route path="/:id" element={<Details />} />
+        {/* */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
